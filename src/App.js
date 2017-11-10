418286dc0868dcbedacea6a6fb96bcb0711f1289
chris martinez <chris martinez>
2017-11-10 00:01:10 -0800

    pass arg/match to audio component, fix bug in switch

--- src/App.js
+++ src/App.js
@@ -1,4 +1,9 @@
 import React, { Component } from 'react';
+import {
+  BrowserRouter,
+  Route,
+  Switch
+} from 'react-router-dom';
 //modules
 import Tuna from 'tunajs';
 import Knob from 'svg-knob';
@@ -8,7 +13,6 @@ import Reverb from './components/reverb.jsx';
 import PingPongDelay from './components/pingPongDelay.jsx';
 import Audio from './components/audio.jsx';
 import './App.css';
-
 
 class App extends Component {
 
@@ -21,111 +25,29 @@ class App extends Component {
       bypass: 0
     }
   }
-  //
-  // createAudioContext = window.AudioContext || window.webkitAudioContext || window.webkitAudioContext;
-  // ctx = new this.createAudioContext();
-  //
-  //
-  // tuna = new Tuna(this.ctx);
-  // chorusNode = new this.tuna.Chorus({
-  //      rate: 1.5,
-  //      feedback: 0.2,
-  //      delay: 0.0045,
-  //      bypass: 0
-  // });
 
-  //
-  // audioFileLoader = () =>{
-  //   // console.log("toot");
-  //     var soundObj = {};
-  //     soundObj.fileDirectory = './sounds/guiltmaster.wav';
-  //     var getSound = new XMLHttpRequest();
-  //     getSound.open("GET", soundObj.fileDirectory, true);
-  //     getSound.responseType = "arraybuffer";
-  //     getSound.onload = () => {
-  //       console.log(getSound.response);
-  //         this.ctx.decodeAudioData(getSound.response, function(buffer) {
-  //           // console.log(buffer);
-  //           console.log(buffer);
-  //             // this.soundObj.soundToPlay = buffer;
-  //         });
-  //     }
-  //     getSound.send();
-  // //       var playSound = this.ctx.createBufferSource();
-  // //       playSound.buffer = soundObj.soundToPlay;
-  // //       playSound.connect(this.ctx.destination);
-  // //       playSound.start(this.ctx.currentTime);
-  // }
-
-  // function loadFile() {
-  //     var req = new XMLHttpRequest();
-  //     req.open("GET","music.mp3",true);
-  //     req.responseType = "arraybuffer";
-  //     req.onload = function() {
-  //         //decode the loaded data
-  //         ctx.decodeAudioData(req.response, function(buffer) {
-  //             buf = buffer;
-  //             play();
-  //         });
-  //     };
-  //     req.send();
-  // }
-  // Once the file is loaded it must be decoded into a raw sound buffer. The code above does this with another callback function. Once decoded we can actually play the sound.
-  //
-  // //play the loaded file
-  // function play() {
-  //     //create a source node from the buffer
-  //     var src = ctx.createBufferSource();
-  //     src.buffer = buf;
-  //     //connect to the final output node (the speakers)
-  //     src.connect(ctx.destination);
-  //     //play immediately
-  //     src.noteOn(0);
-  // }
-  //
-
-
-
-
-
-  // soundObj = this.audioFileLoader();
-  // audioFileLoader();
-
-
-  // {(() => {
-  //     switch(this.featuredEffect) {
-  //         case 'reverb':
-  //             return <Reverb />;
-  //         case 'warning':
-  //             return <Delay />;
-  //         case 'error':
-  //             return <PingPongDelay  />;
-  //         default:
-  //             return null;
-  //     }
-  // })()}
   changeValue = (effect, value) =>{
     var aspects = {
       ...this.state.aspects,
-      [effect]: parseInt(value)
-
+      [effect]: parseFloat(value)
     }
    this.setState({aspects: aspects});
-  //  console.log(this.chorusNodeInit);
   }
-  // rate={this.state.aspects.rate}
-  // delay={this.state.aspects.delay}
-  // bypass={this.state.aspects.bypass}
-  // feedback={this.state.aspects.feedback}
+
+
   render() {
     return (
-      <div >
-        <Audio
-          changeValue={this.changeValue}
-          aspects={this.state.aspects}
-          />
-
-      </div>
+      <BrowserRouter>
+        <Switch>
+          <Route
+            path="/:effectName"
+            render={routeProps=><Audio
+                        {...routeProps}
+                        changeValue={this.changeValue}
+                        aspects={this.state.aspects}
+            />}/>
+        </Switch>
+      </BrowserRouter>
     );
   }
 }
--- src/components/audio.jsx
+++ src/components/audio.jsx
@@ -4,45 +4,118 @@ import audioPath from './tori.mp3'
 import ControlKnob from './controlknob.jsx';
 
 
+
 class Audio extends Component {
 
     createAudioContext = window.AudioContext || window.webkitAudioContext || window.webkitAudioContext;
     ctx = new this.createAudioContext();
     tuna = new Tuna(this.ctx);
-    chorusNodeInit;
+    featuredNode;
 
 
-    sampleData = {
-      rate: {
-        min: 0,
-        max: 8,
-        increments: .01
+    effectParams ={
+      chorus:{
+        rate: {
+          min: 0,
+          max: 8,
+          increments: .01
+        },
+        feedback: {
+          min: 0,
+          max: 10,
+          increments: .1
+        },
+        delay: {
+          min: 0,
+          max: 1,
+          increments: .001
+        },
+        bypass: {
+          min: 0,
+          max: 1,
+          increments: 1
+        }
       },
-      feedback: {
-        min: 0,
-        max: 10,
-        increments: .1
-      },
-      delay: {
-        min: 0,
-        max: 1,
-        increments: .001
-      },
-      bypass: {
-        min: 0,
-        max: 1,
-        increments: 1
+      delay:{
+        feedback: {
+          min:  0,
+          max: 1,
+          increments: 1
+        },
+        delayTime: {
+          min: 1,
+          max: 10000,
+          increments:200,
+        },
+        wetLevel: {
+          min: 0,
+          max: 1,
+          increments: 1
+        },
+        dryLevel: {
+          min: 0,
+          max: 1,
+          increments: 1
+        },
+        cutoff: {
+          min: 20,
+          max: 20000,
+          increments: 500
+        },
+        bypass: {
+          min: 0,
+          max: 0,
+          increments: 0
+        }
       }
     }
+
+
+    // componentDidMount =()=>{
+    //   console.log(this.props.match.params.effectName);
+    // }
+
+    loadTuna = () =>{
+      var tunaFeature = this.props.match.params.effectName;
+      switch (tunaFeature) {
+        case  'chorus':
+        this.featuredNode  = new this.tuna.Chorus({
+             rate: 0,
+             feedback:0,
+             delay: 0,
+             bypass: 0
+        });
+        break;
+        case tunaFeature ==='delay':
+        this.featuredNode  == new this.tuna.Delay({
+                feedback: 0.45,    //0 to 1+
+                delayTime: 150,    //1 to 10000 milliseconds
+                wetLevel: 0.25,    //0 to 1+
+                dryLevel: 1,       //0 to 1+
+                cutoff: 2000,      //cutoff frequency of the built in lowpass-filter. 20 to 22050
+                bypass: 0
+              });
+          break;
+        default:
+        console.log("default");
+        break;
+      }
+
+
+    }
+    ///////_____
+    // this.featuredNode  = new this.tuna.[tunaFeature]({
+    //      rate: this.props.aspects.rate,
+    //      feedback:this.props.aspects.feedback,
+    //      delay: this.props.aspects.delay,
+    //      bypass: this.props.aspects.bypass,
+    // });
+    ///////_____
+
     componentWillReceiveProps = (nextProps) =>{
       Object.keys(nextProps.aspects).map((key, index) => {
-        this.chorusNodeInit[key] = nextProps.aspects[key];
+        this.featuredNode[key] = nextProps.aspects[key];
       });
-      // this.chorusNodeInit = {
-      //   ...this.chorusNodeInit,
-      //   ...nextProps.aspects
-      // }
-      console.log(this.chorusNodeInit);
     }
      audioFileLoader = () =>{
 
@@ -64,22 +137,14 @@ class Audio extends Component {
         soundObj.play = () => {
             var playSound = this.ctx.createBufferSource();
             playSound.buffer = soundObj.soundToPlay;
-            playSound.connect(this.chorusNodeInit);
-            this.chorusNodeInit.connect(this.ctx.destination);
+            playSound.connect(this.featuredNode);
+            this.featuredNode.connect(this.ctx.destination);
             playSound.start()
         }
 
         return soundObj;
     }
- // stuf.play();
- loadTuna = () =>{
-   this.chorusNodeInit  = new this.tuna.Chorus({
-        rate: this.props.aspects.rate,
-        feedback:this.props.aspects.feedback,
-        delay: this.props.aspects.delay,
-        bypass: this.props.aspects.bypass,
-   });
- }
+
  destroy = () => {
  //   this.state.state.chorusNode = false;
  //   console.log(this.state.chorusNode);
@@ -89,20 +154,23 @@ class Audio extends Component {
   }
 
   render() {
+    const featuredEffectName = this.props.match.params.effectName;
+    const featuredEffectParams = this.effectParams[featuredEffectName];
+    console.log(featuredEffectParams);
+
     return (
     <div>
       <button onClick={this.loadTuna}>loadTuna</button>
       <button onClick={this.audioFileLoader}>PLAY</button>
       <button onClick={this.destroy}>DESTROY</button>
-      {Object.keys(this.sampleData).map((key, i)=>
+      {Object.keys(featuredEffectParams).map((key, i)=>
         <ControlKnob  key={key}
                       effect={key}
-                      data={this.sampleData[key]}
+                      data={featuredEffectParams[key]}
                       changeValue={this.changeValue}
           />
         )
       }
-
   </div>
   );
 }
